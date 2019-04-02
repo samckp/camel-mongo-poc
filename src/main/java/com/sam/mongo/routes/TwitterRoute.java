814aeade55d381e9cb050249d9139f237f4e98df
samckp <sam.mca.liet@gmail.com>
2019-04-02 20:33:05 +0530

    secret modified

--- src/main/java/com/sam/mongo/routes/TwitterRoute.java
+++ src/main/java/com/sam/mongo/routes/TwitterRoute.java
@@ -10,10 +10,10 @@ public class TwitterRoute extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        String consumerKey= "z7PCBNmhM6tySoagBMR06ySR3-00";
-        String consumerSecret="Uswp8JfqYnB9Kvu0owfAxiwZNPtovKtvMW0tYj8C9hr7rp1vDx";
-        String accessToken="1113062061183889409-BIMNd7TVRJaZWQ3HeoZ1iO4Asw9P7j";
-        String accessTokenSecret="XcXslKnTxEq8NkPYdd8lOhhax0nPzdyZcuKmbSbURkcKp";
+        String consumerKey= "z7PCBNmhM6tySoSR3";
+        String consumerSecret="Uswp8JfqYnB9KvKtvMW0tYj8C9hr7rp1vDx";
+        String accessToken="11130620611838eoZ1iO4Asw9P7j";
+        String accessTokenSecret="XcXslKnTxEbSbURkcKp";
 
         from("twitter://timeline/home?type=polling&delay=6000" +
                 "&consumerKey=" + consumerKey+
